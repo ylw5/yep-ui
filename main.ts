f641cd8724f55180e5a9a84a2984a2728b08a998
ylw5 <767138650@qq.com>
2023-02-18 23:44:55 +0800

    delete node_modules

--- main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import 'uno.css'
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-import App from './App.vue'
-createApp(App).mount('#app')
