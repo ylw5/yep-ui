cc3491672bf0047de5b29420bfcc8db393cbe4c7
ylw5 <767138650@qq.com>
2023-03-05 23:57:01 +0800

    fix:checkbox export

--- packages/yep-ui/src/components/checkbox/index.ts
+++ packages/yep-ui/src/components/checkbox/index.ts
@@ -1,12 +1,12 @@
-export { default as Indicator } from './Indicator.vue'
-export { default as Group } from './Group.vue'
-export { default as Root } from './Root.vue'
-// export const Checkbox = {
-//   Root,
-//   Indicator,
-//   Group,
-// }
+import Indicator from './Indicator.vue'
+import Group from './Group.vue'
+import Root from './Root.vue'
+export const Checkbox = {
+  Root,
+  Indicator,
+  Group,
+}
 
-// export const CheckboxRoot = Root
-// export const CheckboxIndicator = Indicator
-// export const CheckboxGroup = Group
+export const CheckboxRoot = Root
+export const CheckboxIndicator = Indicator
+export const CheckboxGroup = Group
