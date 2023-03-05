1fb1904138239cc625d7d69238bdf49bba0bf2b7
ylw5 <767138650@qq.com>
2023-03-05 14:52:17 +0800

    fix:ignorecase = false

---
+++ docs/story/checkbox/Group.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const checkedfruits = ref(['apple', 'banana'])
+const fruits = ['apple', 'banana', 'orange']
+</script>
+
+<template>
+  <Checkbox.Group v-model="checkedfruits" class="flex gap-2">
+    <Checkbox.Root
+      v-for="fruit in fruits"
+      :key="fruit"
+      class="checkbox"
+      :value="fruit"
+    />
+  </Checkbox.Group>
+  <p>selected: {{ checkedfruits }}</p>
+</template>
