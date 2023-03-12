a87e90e624239d09f460417224b6a6caf2c5f064
ylw5 <767138650@qq.com>
2023-03-12 17:18:20 +0800

    test:init

---
+++ packages/yep-ui/test/switch.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest'
+import { ref } from 'vue'
+import { mount, shallowMount } from '@vue/test-utils'
+import { Switch } from '../src/components/switch'
+
+describe('Test Switch', () => {
+  it('switch model value', async () => {
+    expect(Switch.Root).toBeTruthy()
+    const modelValue = ref(false)
+    const wrapper = shallowMount(Switch.Root, {
+      props: {
+        'modelValue': modelValue.value,
+        'onUpdate:modelValue': (value: boolean) => {
+          modelValue.value = value
+        },
+      },
+    })
+    await wrapper.trigger('click')
+    expect(modelValue.value).toBe(true)
+  })
+})
--- packages/yep-ui/vite.config.ts
+++ packages/yep-ui/vite.config.ts
@@ -1,53 +1,15 @@
+/// <reference types="vitest" />
 import { URL, fileURLToPath } from 'url'
 import { resolve } from 'path'
-import * as path from 'path'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// export default defineConfig({
-//   build: {
-//     lib: {
-//       entry: resolve(__dirname, 'src/index.ts'),
-//       name: 'yep-ui',
-//       fileName: 'yep-ui',
-//     },
-//     emptyOutDir: false,
-//     rollupOptions: {
-//       external: ['vue'],
-//       output: {
-//         globals: {
-//           vue: 'Vue',
-//         },
-//       },
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url)),
-//     },
-//   },
-//   plugins: [vue()],
-// })
-
-// TODO:多入口打包，支持完整的Tree Shaking
-// function componentsEntry() {
-//   return readdirSync(path.resolve(__dirname, 'src/components')).reduce((entries, componentName) => {
-//     if (componentName === 'index.ts')
-//       return entries
-//     const fullDir = path.resolve(__dirname, 'src/components', componentName, 'index.ts')
-//     entries[componentName] = fullDir
-
-//     return entries
-//   }, {})
-// }
-
 export default defineConfig({
   build: {
     lib: {
-      entry: {
-        checkbox: resolve(__dirname, 'src/components/checkbox/index.ts'),
-      },
-      name: '[name]',
-      fileName: '[name]/index',
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'yep-ui',
+      fileName: 'yep-ui',
     },
     emptyOutDir: false,
     rollupOptions: {
@@ -59,5 +21,17 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+
 })
